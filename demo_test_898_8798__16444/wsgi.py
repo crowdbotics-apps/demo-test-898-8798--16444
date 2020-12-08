d3298e0c7cdc25aaa5d95a51237bca602a8889c7
Crowdbotics <team@crowdbotics.com>
2020-12-08 07:52:41 +0000

    Update models

--- demo_test_898_8798__16444/wsgi.py
+++ demo_test_898_8798__16444/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'demo_test_898_8798__16444.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "demo_test_898_8798__16444.settings")
 
 application = get_wsgi_application()
--- home/models.py
+++ home/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
@@ -6,18 +7,31 @@ from django.db import models
 
 
 class CustomText(models.Model):
-    title = models.CharField(max_length=150)
+    title = models.CharField(
+        max_length=150,
+    )
 
     def __str__(self):
         return self.title
 
     @property
     def api(self):
-        return f'/api/v1/customtext/{self.id}/'
+        return f"/api/v1/customtext/{self.id}/"
 
     @property
     def field(self):
-        return 'title'
+        return "title"
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def api(self):
+        return f"/api/v1/customtext/{self.id}/"
+
+    @property
+    def field(self):
+        return "title"
 
 
 class HomePage(models.Model):
@@ -25,8 +39,47 @@ class HomePage(models.Model):
 
     @property
     def api(self):
-        return f'/api/v1/homepage/{self.id}/'
+        return f"/api/v1/homepage/{self.id}/"
 
     @property
     def field(self):
-        return 'body'
+        return "body"
+
+    @property
+    def api(self):
+        return f"/api/v1/homepage/{self.id}/"
+
+    @property
+    def field(self):
+        return "body"
+
+
+class Payment(models.Model):
+    "Generated Model"
+    email = models.EmailField(
+        max_length=254,
+    )
+    card_number = models.CharField(
+        max_length=16,
+    )
+    expiry_month = models.IntegerField()
+    expiry_year = models.IntegerField()
+    cvv = models.CharField(
+        max_length=4,
+    )
+    name = models.CharField(
+        max_length=50,
+    )
+    address = models.TextField()
+    city = models.CharField(
+        max_length=50,
+    )
+    state = models.CharField(
+        max_length=50,
+    )
+    zip = models.CharField(
+        max_length=10,
+    )
+    country = models.CharField(
+        max_length=50,
+    )
